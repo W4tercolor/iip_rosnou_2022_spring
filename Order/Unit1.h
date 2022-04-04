b6430c0975f195df872357c26389a0e9d659db04
Watercolor <EmelyanovaPA@stud.rosnou.ry>
2022-04-04 14:40:19 +0300

    order 0.3
    lдобавлены кнопки сохранения и открытие файла. А также верхнее меню

--- Order/Unit1.h
+++ Order/Unit1.h
@@ -8,6 +8,9 @@
 #include <Vcl.StdCtrls.hpp>
 #include <Vcl.Forms.hpp>
 #include <Vcl.Buttons.hpp>
+#include <Vcl.Dialogs.hpp>
+#include <Vcl.ImgList.hpp>
+#include <Vcl.Menus.hpp>
 #include <vector>
 #include <String.h>
 //---------------------------------------------------------------------------
@@ -42,11 +45,23 @@ __published:	// IDE-managed Components
 	TButton *ButtonAdd;
 	TButton *ButtonDelete;
 	TEdit *EditOrder;
+	TOpenDialog *OpenDialog1;
+	TSaveDialog *SaveDialog1;
+	TImageList *ImageList1;
+	TMainMenu *MainMenu1;
+	TMenuItem *N1;
+	TMenuItem *ButtonSave1;
+	TMenuItem *ButtonOpen1;
+	TButton *ButtonSave;
 	void __fastcall ShowRecord();
 	void __fastcall ClearFields();
 	void __fastcall ButtonAddClick(TObject *Sender);
 	void __fastcall ButtonLeftClick(TObject *Sender);
 	void __fastcall ButtonRightClick(TObject *Sender);
+	void __fastcall ButtonSaveClick(TObject *Sender);
+	void __fastcall ButtonOpenClick(TObject *Sender);
+	void __fastcall ButtonSave1Click(TObject *Sender);
+	void __fastcall ButtonOpen1Click(TObject *Sender);
 private:	// User declarations
 public:		// User declarations
 	__fastcall TForm1(TComponent* Owner);
